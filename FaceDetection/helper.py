b0f611bfa5b46f8a97404f8a4c06b32cea75888f
Sanam Narula <s5narula@uwaterloo.ca>
2015-09-09 22:09:26 -0400

    Cleaning Project
    Cleaning Project

--- FaceDetection/helper.py
+++
@@ -1,4 +0,0 @@
-__author__ = 's5narula'
-current_entry_value = "Hello"
-current_status = 0
-interim_code = ""
--- FaceDetection/lock_screen.py
+++
@@ -1,8 +0,0 @@
-import pyHook
-from pyHook.example import OnKeyboardEvent
-import pygame
-from Tkinter import *
-import tkMessageBox as tm
-from FaceDetection import facedetection
-from FaceDetection import helper
-
